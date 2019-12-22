1a5ba58461e8b449bc46840e9928b6afcae00928
Mikael Fredriksson <frimurare@gmail.com>
2019-12-22 10:40:55 +0100

    Introducing ZMQ as communication layer between emulator and terminal server, removing hard dependency.

---
+++ TerminalServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+
+namespace Core8
+{
+    public class Program
+    {
+        private static LoggingLevelSwitch loggingLevel = new LoggingLevelSwitch(Serilog.Events.LogEventLevel.Information);
+
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.ControlledBy(loggingLevel)
+                .CreateLogger();
+
+        }
+
+    }
+}
