2bcca6897890f130bfc02b6b102fe4005e86ac9f
Mikael Fredriksson <frimurare@gmail.com>
2020-01-26 15:40:12 +0100

    Basic JMP-JMS tests

---
+++ Tests/Core8Tests/MAINDEC/BasicJMPJMSTests.cs
@@ -0,0 +1,28 @@
+using Core8.Tests.MAINDEC.Abstract;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Core8.Tests.MAINDEC
+{
+    [TestClass]
+    public class BasicJMPJMSTests : MAINDECTestsBase
+    {
+        protected override string TapeName => @"MAINDEC/tapes/MAINDEC-8E-D0IB-PB.bin";
+
+        protected override string[] ExpectedOutput => new[] { "\u0007\u0007\u0007\u0007\u0007" };
+
+        protected override TimeSpan MaxRunningTime => TimeSpan.FromSeconds(10);
+
+        [TestMethod]
+        public void RunTest()
+        {
+            PDP.Load8(0200);
+
+            PDP.Toggle8(0000);
+
+            PDP.Clear();
+
+            StartAndWaitForCompletion();
+        }
+    }
+}
