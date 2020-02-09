51ebf7e5d72c8fdea1c0b1b74586de6ab5137746
Mikael Fredriksson <frimurare@gmail.com>
2020-02-09 13:10:42 +0100

    Added command-line args parser

---
+++ Emulator/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace Core8
+{
+    public class Options
+    {
+        [Option(Required = false, Default = false, HelpText = "play TINT")]
+        public bool TINT { get; set; }
+
+        [Option(Required = false, Default = @"c:\bin\palbart\palbart", HelpText = "location of PALBART (required for asm)")]
+        public string PALBART { get; set; }
+
+        [Option(Required = false, HelpText = "assemble and run assembler file")]
+        public string Assemble { get; set; }
+
+        [Option(Required = false, Default ="0200", HelpText = "starting address")]
+        public string StartingAddress { get; set; }
+    }
+}
--- Emulator/Program.cs
+++ Emulator/Program.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CommandLine;
 using Serilog;
 using Serilog.Core;
-using System;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
 
 namespace Core8
@@ -14,30 +11,28 @@ namespace Core8
 
         private static HttpClient httpClient = new HttpClient();
 
-        private static IConfigurationRoot configuration;
-
         public static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
-
-            configuration = builder.Build();
-
-            var pdp = new PDP();
-
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
                 .WriteTo.File("emulator.log")
                 .MinimumLevel.ControlledBy(loggingLevel)
                 .CreateLogger();
 
-            if (args.Any(x => x == "--tint"))
-            {
-                PlayTINT(pdp);
-            }
+            Parser.Default.ParseArguments<Options>(args)
+                    .WithParsed<Options>(o =>
+                    {
+                        if (o.TINT)
+                        {
+                            PlayTINT();
+                        }
+                    });
         }
 
-        public static void PlayTINT(PDP pdp)
+        public static void PlayTINT()
         {
+            var pdp = new PDP();
+
             pdp.Clear();
 
             pdp.LoadPaperTape(httpClient.GetByteArrayAsync(@"https://github.com/PontusPih/TINT8/releases/download/v0.1.0-alpha/tint.bin").Result);
